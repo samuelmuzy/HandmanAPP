689e25c046f8de26f8b3487324d6947ffcf8ded6
GabrielSetta <devsetta@gmail.com>
2025-05-31 11:24:06 -0300

    Splash Screen animada e configura navegação inicial
    - Criação do componente SplashScreen.
    - Adição de animação de pulso ao texto "HANDYMAN" usando React Native Animated.
    - Configuração do RootNavigation para iniciar com a SplashScreen.
    - Implementação de um temporizador na SplashScreen para navegar automaticamente para o AuthStack após 3 segundos.
    
    Esta alteração estabelece a tela de splash como ponto de entrada do aplicativo, seguida pela tela de login/cadastro.

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,15 +1,28 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthStackNavigation} from "./AuthStackNavigation";
-import { useContext } from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthStackNavigation } from "./AuthStackNavigation";
+import SplashScreen from '../pages/SplashScreen';
 import { TabNavigation } from "./TabNavigation";
 import { useAuth } from "../context/AuthContext";
 
+export type RootStackParamList = {
+  Splash: undefined;
+  AuthStack: undefined;
+  AppStack: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 export const RootNavigation = () =>{
     const { isAuthenticated } = useAuth();
-    console.log('isAuthenticated:', isAuthenticated);
+    
     return(
         <NavigationContainer>
-            {isAuthenticated ? <TabNavigation /> : <AuthStackNavigation />}
+            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
+                <Stack.Screen name="Splash" component={SplashScreen} />
+                <Stack.Screen name="AuthStack" component={AuthStackNavigation} />
+                <Stack.Screen name="AppStack" component={TabNavigation} />
+            </Stack.Navigator>
         </NavigationContainer>
     )
 }
